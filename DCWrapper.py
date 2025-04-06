3d6832186c0de94a42554db1fbcf4b431aafc648
xyfuture <xyfuture@foxmail.com>
2025-04-06 16:02:03 +0800

    first commit

---
+++ DCWrapper.py
@@ -0,0 +1,19 @@
+
+
+class Design:
+    def __init__(self):
+        
+        self.file_name_list:list[str] = []
+        self.top_name :str = ""
+
+        
+        self.clk_name :str = ""
+        
+        self.switch_activity_list:list = [] 
+
+        
+        # pdk 
+        pass 
+
+
+
