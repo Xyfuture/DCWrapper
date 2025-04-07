b9f30d10e950ec8a1f27165d445610b37f179f06
xyfuture <xyfuture@foxmail.com>
2025-04-07 20:26:53 +0800

    fix bug

--- DCWrapper/DCWrapper.py
+++ DCWrapper/DCWrapper.py
@@ -1,3 +1,4 @@
+import inspect
 import os
 import shutil
 from datetime import datetime
@@ -27,6 +28,7 @@ class Design:
 
         self.work_path:str = ""
 
+        self.base_workspace:str = ""
 
         self.tcl_template:list = [
             self.gen_workspace_config,
@@ -48,24 +50,28 @@ class Design:
 
 
     def set_pdk(self,target_library:str,link_library:str='',symbol_library:str=''):
-        def path_helper(file_path:str):
-            if os.path.isabs(file_path):
-                return file_path
-            else:
-                base_path = os.path.dirname(os.path.abspath(__file__))
-                file_path = os.path.join(base_path,file_path)
 
-                return file_path
-
-        self.target_library = path_helper(target_library)
+        self.target_library = target_library
         if link_library:
-            self.link_library = path_helper(link_library)
+            self.link_library = link_library
         if symbol_library:
-            self.symbol_library = path_helper(symbol_library)
+            self.symbol_library = symbol_library
 
 
     def set_design(self,file_name_list:list[str],top_name:str):
         self.file_name_list = file_name_list
+
+        tmp_file_name = self.file_name_list[0]
+        if tmp_file_name.endswith('.v'):
+            self.file_type = 'verilog'
+        elif tmp_file_name.endswith('.sv'):
+            self.file_type = 'sverilog'
+        elif tmp_file_name.endswith('.vhd'):
+            self.file_type = 'vhdl'
+        else:
+            raise ValueError
+
+
         self.top_name = top_name
 
 
@@ -77,8 +83,8 @@ class Design:
         pass 
 
 
-    def set_workspace(self):
-        pass
+    def set_base_workspace(self,base_workspace:str = ''):
+        self.base_workspace = base_workspace
 
 
     def add_switching_activity(self,port_name:str,toggle_rate:int,static_probability:int):
@@ -86,24 +92,58 @@ class Design:
             (port_name,toggle_rate,static_probability)
         )
 
+    def add_multi_switching_activity(self,port_name_list:list[str],toggle_rate:int,static_probability:int):
+        for port_name in port_name_list:
+            self.switch_activity_list.append(
+                (port_name,toggle_rate,static_probability)
+            )
+
+
     def add_case_activity(self,port_name:str,case_value:int):
         self.case_list.append(
             (port_name,case_value)
         )
+        
+    def add_multi_case_activity(self,port_name_list:list[str],case_value:int):
+        for port_name in port_name_list:
+            self.case_list.append(
+                (port_name,case_value)
+            )
 
 
 
     def gen_workspace_config(self):
         s = f'set search_path "{self.work_path}"\n'
-        s += f'set report_path "{os.path.join(self.work_path,"report")}" '
+        s += f'set report_path "{os.path.join(self.work_path,"report")}" \n'
+        s += f'define_design_lib work -path "{os.path.join(self.work_path,"design")}" \n'
 
         return s
 
 
     def gen_pdk_config(self):
-        s =  f'set target_library "{self.target_library}"\n'
-        s += f'set link_library "{self.target_library}"\n'
-        s += f'set symbol_library "{self.symbol_library}"\n' 
+
+        def path_helper(file_path:str):
+            if os.path.isabs(file_path):
+                return file_path
+            else:
+                if self.base_workspace:
+                    base_path = self.base_workspace
+                else:
+                    base_path = os.getcwd()
+                file_path = os.path.join(base_path,file_path)
+                file_path = os.path.normpath(file_path)
+
+                return file_path
+
+        s =  f'set target_library "{path_helper(self.target_library)}"\n'
+
+        if self.link_library:
+            s += f'set link_library "{path_helper(self.link_library)}"\n'
+        else:
+            s += f'set link_library "{path_helper(self.target_library)}" \n'
+
+        if self.symbol_library:
+            s +=f'set symbol_library "{path_helper(self.symbol_library)}"\n'
 
         return s
 
@@ -139,26 +179,38 @@ class Design:
 
     def gen_report(self):
         s =  f'report_power > $report_path/power.rpt \n'
-        s += f'report_power > $report_path/power_hierarchy.rpt \n'
+        s += f'report_power -hierarchy > $report_path/power_hierarchy.rpt \n'
         s += f'report_area > $report_path/area.rpt \n'
-        s += f'report_area > $report_path/area_hierarchy.rpt \n'
+        s += f'report_area -hierarchy > $report_path/area_hierarchy.rpt \n'
         s += f'report_timing > $report_path/timing.rpt \n'
 
-        s += f'report_hierarchy -format full > $report_path/hierarchy.rpt \n'
+        s += f'report_hierarchy  > $report_path/hierarchy.rpt \n'
         s += f'report_design -verbose > $report_path/design.rpt \n'
         s += f'report_saif > $report_path/saif.rpt \n'
+
+        s += f'check_design > $report_path/check_design.rpt \n'
         return s 
 
 
     def generate_tcl_file(self):
         for f in self.tcl_template:
             self.tcl_text += f()
+            self.tcl_text += '\n\n'
 
-        # TODO write to a tcl file 
         with open(os.path.join(self.work_path,'dc.tcl'),'w') as f:
-            f.write('# generated by DCWrapper at time {datetime.now().strftime("%y-%m-%d_%H-%M")}\n')
+            f.write(f'# generated by DCWrapper at time {datetime.now().strftime("%y-%m-%d_%H-%M")}\n')
             f.write(self.tcl_text)
-            
+
+        # Copy source RTL file  to work path
+        for file_name in self.file_name_list:
+            if self.base_workspace:
+                base_path = self.base_workspace
+            else:
+                base_path = os.getcwd()
+            file_path = os.path.join(base_path, file_name)
+
+            if os.path.isfile(file_path):
+                shutil.copy(file_path, self.work_path)
 
     def check_design(self)->bool:
         pass 
@@ -174,14 +226,11 @@ class Design:
         else:
             os.makedirs(self.work_path)
             os.makedirs(os.path.join(self.work_path,'report'))
-    
-        for file_name in self.file_name_list:
-            file_path = os.path.join(current_path,file_name)
-            
-            if os.path.isfile(file_path):
-                shutil.copy(file_path, self.work_path)
-            
-        shutil.copy(os.path.realpath(__file__),self.work_path)
+            os.makedirs(os.path.join(self.work_path,'design'))
+
+        caller_frame = inspect.stack()[-1]
+        caller_file_name = caller_frame.filename
+        shutil.copy(os.path.realpath(caller_file_name),self.work_path)
 
             
             
